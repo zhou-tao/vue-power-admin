15bd87404cf8e6fb1476647f4fbc4319e822a4f7
zhou-tao <zhoutao_233@163.com>
2022-07-29 14:53:34 +0800

    fix: element-plus ElMessage style error

--- src/components/register.ts
+++ src/components/register.ts
@@ -1,1 +1,8 @@
-export {}
+import type { App } from 'vue'
+// 使用element-plus 自动导入需要手动引入message、loading 样式
+import 'element-plus/theme-chalk/el-loading.css'
+import 'element-plus/theme-chalk/el-message.css'
+
+export function registerGlobComp(app: App) {
+  console.log('register all component in there', app)
+}
--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import '@/styles/index.less'
 import 'virtual:windi-utilities.css'
 import App from './App.vue'
 import { createApp } from 'vue'
-// import { registerGlobComp } from '@c/register'
+import { registerGlobComp } from '@c/register'
 import { setupStore } from '@/store'
 import { router, setupRouter } from '@/router'
 import { setupRouterGuard } from '@/router/guard'
 
 const app = createApp(App)
 
-// 注册全局组件
-// registerGlobComp(app)
+// 注册全局组件或样式引入
+registerGlobComp(app)
 
 // 配置 store
 setupStore(app)
