68f6f0639896338c5e31be82e5511cbfa12a5f6e
zhou-tao <zhoutao_233@163.com>
2022-07-30 22:02:33 +0800

    feat(login): use element-plus rewrite

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,38 +1,38 @@
 <script setup lang="ts" name="Login">
-  import { reactive } from 'vue'
+  import config from '@/config'
   import { LoginParams } from '@/api/_auth/model'
   import { useUserStore } from '@/store/modules/user'
   import { useMessage } from '@/hooks/web/useMessage'
-  import config from '@/config'
+  import type { FormInstance, FormRules } from 'element-plus'
 
-  const message = useMessage()
+  const { $message } = useMessage()
   const loginForm = reactive<LoginParams>({
     username: '',
     password: '',
     rememberMe: false
   })
-  const loginRules = {
+  const loginFormRef = ref<FormInstance>()
+  const loginRules = reactive<FormRules>({
     username: [{ required: true, message: '请输入登录账号', trigger: 'blur' }],
     password: [{ required: true, message: '请输入登录密码', trigger: 'blur' }]
-  }
+  })
   let loginStatus = $ref<boolean>(false)
 
-  async function doLogin(value: LoginParams) {
-    const userStore = useUserStore()
-    try {
-      loginStatus = true
-      // createMessage.loading({
-      //   duration: 0,
-      //   content: '正在登录',
-      //   key: MessageKey.LOADING_KEY
-      // })
-      await userStore.login(value)
-      message.success({
-        message: '登录成功'
-      })
-    } finally {
-      loginStatus = false
-    }
+  async function doLogin(formEl: FormInstance | undefined) {
+    if (!formEl) return
+    await formEl.validate(async valid => {
+      if (!valid) return false
+      const userStore = useUserStore()
+      try {
+        loginStatus = true
+        await userStore.login(loginForm)
+        $message.success({
+          message: '登录成功'
+        })
+      } finally {
+        loginStatus = false
+      }
+    })
   }
 </script>
 
@@ -47,44 +47,42 @@
         {{ config.APP.title }}
       </h1>
       <div class="mt-16 flex flex-col -2xl:mt-11">
-        <a-form
-          layout="vertical"
-          :model="loginForm"
-          :rules="loginRules"
-          @finish="doLogin"
-        >
-          <a-form-item name="username" label="账号">
-            <a-input
-              v-model:value="loginForm.username"
-              placeholder="请输入账号"
-            />
-          </a-form-item>
-          <a-form-item
-            name="password"
+        <el-form ref="loginFormRef" :model="loginForm" :rules="loginRules">
+          <el-form-item prop="username" label="账号">
+            <el-input v-model="loginForm.username" placeholder="请输入账号" />
+          </el-form-item>
+          <el-form-item
+            prop="password"
             label="密码"
             :style="{ marginTop: '38px' }"
           >
-            <a-input-password
-              v-model:value="loginForm.password"
+            <el-input
+              v-model="loginForm.password"
+              type="password"
               placeholder="请输入密码"
+              show-password
             />
-          </a-form-item>
-          <a-form-item>
+          </el-form-item>
+          <el-form-item>
             <div
               class="w-360px flex justify-between items-center text-[#a5adc7] text-13px"
             >
-              <a-checkbox v-model="loginForm.rememberMe">记住密码</a-checkbox>
-              <span class="cursor-pointer" @click="message.warning('敬请期待!')"
+              <el-checkbox v-model="loginForm.rememberMe">记住密码</el-checkbox>
+              <span class="cursor-pointer" @click="$message.info('敬请期待！')"
                 >忘记密码？</span
               >
             </div>
-          </a-form-item>
-          <a-form-item :style="{ marginTop: '50px' }">
-            <a-button type="primary" :loading="loginStatus" html-type="submit"
-              >登录</a-button
+          </el-form-item>
+          <el-form-item :style="{ marginTop: '50px' }">
+            <el-button
+              type="primary"
+              :loading="loginStatus"
+              @click="doLogin(loginFormRef)"
             >
-          </a-form-item>
-        </a-form>
+              登录</el-button
+            >
+          </el-form-item>
+        </el-form>
       </div>
     </div>
   </div>
