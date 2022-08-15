3966dd3b49856a2c97fe26a44edbe10bf911c7cb
zhou-tao <zhoutao_233@163.com>
2022-08-15 15:11:35 +0800

    build(cz-git): :package: add cz-git

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,26 @@
+const fs = require('fs')
+const path = require('path')
+const { execSync } = require('child_process')
+
+// 读取src下顶层目录作为默认scope选项（api、assets、components...）
+const scopes = fs
+  .readdirSync(path.resolve(__dirname, 'src'), { withFileTypes: true })
+  .filter(dirent => dirent.isDirectory())
+  .map(dirent => dirent.name.replace(/s$/, ''))
+
+// 获取git状态
+const gitStatus = execSync('git status --porcelain || true')
+  .toString()
+  .trim()
+  .split('\n')
+
+// precomputed scope
+const scopeComplete = gitStatus
+  .find(r => ~r.indexOf('M  src'))
+  ?.replace(/\//g, '%%')
+  ?.match(/src%%((\w|-)*)/)?.[1]
+  ?.replace(/s$/, '')
+
 module.exports = {
   ignores: [commit => commit.includes('init')],
   extends: ['@commitlint/config-conventional'],
@@ -12,22 +35,27 @@ module.exports = {
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'perf',
-        'style',
-        'docs',
-        'test',
-        'refactor',
-        'build',
-        'ci',
-        'chore',
-        'revert',
-        'wip',
-        'workflow',
-        'types',
-        'release'
+        'feat', //新增功能
+        'fix', //修复BUG
+        'perf', //性能优化
+        'style', //代码格式
+        'docs', //文档变更
+        'test', //添加测试或测试改动
+        'refactor', //代码重构
+        'build', //构建相关、外部依赖变更(如升级 npm 包、修改打包配置等)
+        'ci', //修改 CI 配置、脚本
+        'chore', //架构变动
+        'revert', //代码回退
+        'wip', //正在开发中
+        'workflow', //工作流修改
+        'types' //类型定义文件修改
       ]
     ]
+  },
+  prompt: {
+    useEmoji: true,
+    customScopesAlign: !scopeComplete ? 'top' : 'bottom',
+    defaultScope: scopeComplete,
+    scopes: [...scopes, 'mock']
   }
 }
