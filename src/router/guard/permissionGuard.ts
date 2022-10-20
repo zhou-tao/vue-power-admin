efc815387f23b69df3637bc61fdbdae73defc354
zhou-tao <zhoutao_233@163.com>
2022-10-20 10:50:59 +0800

    refactor(util): :recycle: replace iconsole with class BConsole

--- src/router/guard/permissionGuard.ts
+++ src/router/guard/permissionGuard.ts
@@ -6,11 +6,11 @@ import {
   checkAccessToken,
   isRequiresAuthRoute
 } from '@/router/helper'
-import { iconsole } from '@/utils/console'
+import { BConsole } from '@/utils/console'
 import { ConsoleTypeEnum } from '@/enums/consoleEnum'
 
 export const createPermissionGuard = (router: Router) => {
-  iconsole.SUCCESS(ConsoleTypeEnum.ROUTER, 'create permission guard')
+  BConsole.SUCCESS(ConsoleTypeEnum.ROUTER, 'create permission guard')
   router.beforeEach(async (to, from, next) => {
     const userStore = useUserStore()
     const menuStore = useMenuStore()
--- src/utils/console.ts
+++ src/utils/console.ts
@@ -2,32 +2,33 @@ import { ConsoleTypeEnum, ConsoleThemeEnum } from '@/enums/consoleEnum'
 
 const BASIC_STYLE = 'border-radius: 0 20px 20px 0; color: #fff'
 
-export const iconsole = {
-  INFO(type: ConsoleTypeEnum, title: string, content = '') {
+// 自定义 console
+export class BConsole {
+  static INFO(type: ConsoleTypeEnum, title: string, content = '') {
     console.log(
       `%c [${type}] ${title} `,
       `${ConsoleThemeEnum.INFO}${BASIC_STYLE}`,
       content
     )
-  },
+  }
 
-  SUCCESS(type: ConsoleTypeEnum, title: string, content = '') {
+  static SUCCESS(type: ConsoleTypeEnum, title: string, content = '') {
     console.log(
       `%c [${type}] ${title} `,
       `${ConsoleThemeEnum.SUCCESS}${BASIC_STYLE}`,
       content
     )
-  },
+  }
 
-  WARN(type: ConsoleTypeEnum, title: string, content = '') {
+  static WARN(type: ConsoleTypeEnum, title: string, content = '') {
     console.log(
       `%c [${type}] ${title} `,
       `${ConsoleThemeEnum.WARN}${BASIC_STYLE}`,
       content
     )
-  },
+  }
 
-  ERROR(type: ConsoleTypeEnum, title: string, content = '') {
+  static ERROR(type: ConsoleTypeEnum, title: string, content = '') {
     console.log(
       `%c [${type}] ${title} `,
       `${ConsoleThemeEnum.ERROR}${BASIC_STYLE}`,
