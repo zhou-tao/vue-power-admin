323ba39e21893cdaf3530a2413d3a74c6f8aa4cc
zhou-tao <zhoutao_233@163.com>
2022-12-30 17:14:33 +0800

    refactor(view): :recycle: 404 page rewrite using unocss

--- src/views/error/PageNotFound.vue
+++ src/views/error/PageNotFound.vue
@@ -1,30 +1,20 @@
 <script setup lang="ts" name="PageNotFound">
+  import { ArrowLeftBold } from '@element-plus/icons-vue'
   import { useRouter } from 'vue-router'
   const router = useRouter()
 </script>
 
 <template>
-  <div class="h-full bg-white flex justify-center items-center">
-    <div class="w-2/5 flex justify-center">
-      <img src="@/assets/images/boat.png" alt="banner" class="w-[78%]" />
-    </div>
-    <div class="w-2/5 flex flex-col">
-      <img src="@/assets/images/404.png" alt="404" class="w-3/5" />
-      <h2 class="mt-60px text-4xl text-[#5A6275] tracking-8px">
-        糟糕，页面飘走了...
-      </h2>
-      <span class="text-xl text-[#b1b1b1] tracking-2px"
-        >THE PAGE YOU ARE LOOKING FOR NOT AVAILABLE!</span
-      >
-      <a-button type="primary" @click="router.replace('/login?redirect=404')">
-        返回首页
-      </a-button>
-    </div>
+  <div h-full bg-white flex flex-col justify-center items-center>
+    <img src="@/assets/images/not_found.svg" width="600" alt="not_found" />
+    <el-button
+      mt-12
+      round
+      size="large"
+      type="primary"
+      :icon="ArrowLeftBold"
+      @click="router.replace('/login?redirect=404')"
+      >返回首页</el-button
+    >
   </div>
 </template>
-
-<style lang="scss" scoped>
-  :deep(.ant-btn) {
-    @apply mt-24 w-48 h-12 text-xl tracking-3px;
-  }
-</style>
