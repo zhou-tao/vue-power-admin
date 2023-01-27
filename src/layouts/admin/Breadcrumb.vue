ae9e0bcd57af5fcdb2445a0ac6350c6436fd9523
zhou-tao <zhoutao_233@163.com>
2023-01-27 17:47:41 +0800

    feat(layout): :sparkles: add breadcrumb transition

--- src/layouts/admin/Breadcrumb.vue
+++ src/layouts/admin/Breadcrumb.vue
@@ -1,12 +1,19 @@
-<script setup lang="ts" name="BreadCrumb"></script>
+<script setup lang="ts" name="BreadCrumb">
+  const route = useRoute()
+  const matched = computed(() => route.matched.filter(r => r?.meta?.title))
+</script>
 
 <template>
   <el-breadcrumb separator="/">
-    <el-breadcrumb-item :to="{ path: '/admin/dashboard' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item
-    >用户管理</el-breadcrumb-item
-    >
-    <el-breadcrumb-item>菜单管理</el-breadcrumb-item>
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item
+        v-for="r in matched"
+        :key="r.path"
+        v-bind="r.path !== route.path ? { to: r.path } : {}"
+      >
+        {{ r.meta.title }}
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
