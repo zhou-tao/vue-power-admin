64e5f9d554024ce392b0724af1863e6b2dd9ca51
zhou-tao <zhoutao_233@163.com>
2023-03-06 23:24:53 +0800

    feat(component): :sparkles: add image previewer

---
+++ src/utils/previewer.ts
@@ -0,0 +1,20 @@
+// 图片预览器
+import 'viewerjs/dist/viewer.css'
+import Viewer from 'viewerjs'
+
+export default class Previewer {
+  private instance: Viewer
+
+  constructor(el: HTMLElement) {
+    const viewer = new Viewer(el)
+    this.instance = viewer
+  }
+
+  getInstance() {
+    return this.instance
+  }
+
+  setOptions (opts: Record<string, any>) {
+    Viewer.setDefaults(opts)
+  }
+}
--- src/views/admin/feat/image-preview/index.vue
+++ src/views/admin/feat/image-preview/index.vue
@@ -1,8 +1,26 @@
 <script setup lang="ts" name="ImagePreview">
+  import Previewer from '@/utils/previewer'
+
+  const images = ref<HTMLElement>()
+
+  onMounted(() => {
+    new Previewer(images.value!)
+  })
 </script>
 
 <template>
-  <div page-card>ImagePreview</div>
+  <div page-card>
+    <PageTitle description="基于Viewer.js封装的图片预览插件，简单易用。" />
+    <ul ref="images" list-none p="0" m="0">
+      <li><img src="https://picsum.photos/seed/picsum/240/120?random=1" alt="Picture 1" /></li>
+      <li><img src="https://picsum.photos/seed/picsum/240/120?random=2" alt="Picture 2" /></li>
+      <li><img src="https://picsum.photos/seed/picsum/240/120?random=3" alt="Picture 3" /></li>
+    </ul>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+img {
+  @apply cursor-pointer;
+}
+</style>
