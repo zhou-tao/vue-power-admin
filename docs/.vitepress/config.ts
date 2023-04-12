6730e8781b60cc26b8481c376aef084e546a5b69
zhou-tao <zhoutao_233@163.com>
2023-04-12 23:41:31 +0800

    docs: update github link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       }
     ],
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/zhou-tao/vue-power-admin' }
     ]
   }
 })
