281555386def5beb9e693633ca673846f08f05ca
zhou-tao <zhoutao_233@163.com>
2023-04-15 22:04:34 +0800

    fix(server): import for mongoose module

--- apps/server/src/user/user.module.ts
+++ apps/server/src/user/user.module.ts
@@ -1,8 +1,11 @@
+import { MongooseModule } from '@nestjs/mongoose'
 import { Module } from '@nestjs/common'
 import { UserController } from './user.controller'
 import { UserService } from './user.service'
+import { User, UserSchema } from './schemas/user.schema'
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: User.name, schema: UserSchema }])],
   controllers: [UserController],
   providers: [UserService],
 })
