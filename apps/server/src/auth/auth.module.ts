f2607d345d6c973e0efe72f90279acf3982d8ee6
zhou-tao <zhoutao_233@163.com>
2023-04-20 23:31:51 +0800

    fix(server): :bug: reuse userService in auth module

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
-import { UserService } from '../user/user.service'
+import { UserModule } from '../user/user.module'
 
 @Module({
-  imports: [UserService],
+  imports: [UserModule],
   controllers: [AuthController],
   providers: [AuthService]
 })
